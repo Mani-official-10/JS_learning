7ad80c590f4e23a34b2b2bb5ccaaf71baa8fe6b3
Mani-official-10 <mani1993gusain@gmail.com>
2024-08-14 15:35:33 +0530

    arrays new update

--- arrays.js
+++ arrays.js
@@ -1,45 +1,52 @@
 const myarray = [1,2,3,4,5,6, true , "Manish"];
 // A datatype in which multiple datatype can be stored inside the squre brcts 
+// Arrays create the shallow copies rather then the deep copies.
+
+// const arr = new Array(1,2,3,4,5,6); 
+// to construct the array.
+
+myarray.push("Vamika");               /* **** add the value in last of the Array.*/
+myarray.pop();                        /* ******** delete the last index value */
+myarray.shift();                      /* ******** Delete the first value from the array */
+myarray.unshift(4);                   /* ******** add the value in the starting of the array */
 
 
-// myarray.push("Vamika");  **** add the value in last of the Array.
-// myarray.pop();  ******** delete the last index value 
-// myarray.shift();  ******** Delete the first value from the array
-// myarray.unshift(4); ******** add the value in the starting of the array
+console.log(myarray);
+console.log(myarray.includes(9));     /*checks for the included value in array */
+console.log(myarray.indexOf(true));   /*checks for the index of desired value in array */      
+console.log(myarray);
+
+const myarr = myarray.join();          /* join the arrays and convert it into the string */
+console.log(myarr);                    /* default array */                         
+console.log(typeof myarr);             /* string */
+
+/* splice & slice */
+/*   const myarray = [1,2,3,4,5,6, true , "Manish"]; */
+console.log("A",myarray);
+console.log(myarray.slice(1,3)); /* ******* slice prints the new array start from index 1 and exclude index 3 without maipulating the original array. */
+console.log("B",myarray);
 
 
-// console.log(myarray);
-// console.log(myarray.includes(9));
-// console.log(myarray.indexOf(true));
-// console.log(myarray);
+console.log(myarray.splice(1,3)); /* ****** splice prints the new array starts from index 1 and also include index 3 but it manipulate the original array and cut the spliced values.*/
+console.log("C",myarray);
 
-// const myarr = myarray.join();
-// console.log(myarr);
-// console.log(typeof myarr);
+const marvel_heroes = ["Thor","Ironman","Spideman"]
+const dc_heroes = ["Superman","Aquaman","Batman"]
 
-// console.log("A",myarray);
-// console.log(myarray.slice(1,3));  ******* slice prints the new array start from index 1 and exclude index 3 without maipulating the original array. 
-// console.log("B",myarray);
-// console.log(myarray.splice(1,3));  ****** splice prints the new array starts from index 1 and also include index 3 but it manipulate the original array and cut the splaiced values.
-// console.log("C",myarray);
+const all_heroes = marvel_heroes.concat(dc_heroes); /* ***** to add two arrays together in new array*/
+console.log(all_heroes);
 
-// const marvel_heroes = ["Thor","Ironman","Spideman"]
-// const dc_heroes = ["Superman","Aquaman","Batman"]
-
-// const all_heroes = marvel_heroes.concat(dc_heroes);  ***** to add two arrays together in one new array
-// console.log(all_heroes);
-
-// const all_new_heroes = [...all_heroes,...dc_heroes];
-// console.log(all_new_heroes);
+const all_new_heroes = [...all_heroes,...dc_heroes];  /* spread operator works same as concat but mostly used when we need to add more then 2 arrays */
+console.log(all_new_heroes);
 
 const another_array = [1,2,3,[4,5,6],7,8,9,[10,11]];
 
 const spread_another_array = another_array.flat(2);
-// console.log(spread_another_array);
+console.log(spread_another_array);
 
-// console.log(Array.isArray("manish"));
-// console.log(Array.from("manish"));
-// console.log(Array.from({name:"manish"}));
+console.log(Array.isArray("manish"));              /* check for the array */
+console.log(Array.from("manish"));                 /* convert to array from string */
+console.log(Array.from({name:"manish"}));          /* empty array returned */
 
 let score1 = 100;
 let score2 = 200;
---
+++ objects3.js
@@ -0,0 +1,33 @@
+const obj   = {
+    courseName : "ABC",
+    Instructor : "Manish",
+    Fee : 999
+}
+
+// console.log(obj.Fee);   normal way of print values.
+
+
+//  Object destructuring
+
+const {Fee} = obj;
+
+console.log(Fee);
+
+// API's form 
+
+// in form of JSON
+{
+    "name":"Manish",
+    "age": 23,
+    "sex": "Male"
+}
+
+// in form of Array
+
+[
+    {},
+    {},
+    {}
+]
+
+
