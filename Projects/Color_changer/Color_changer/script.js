de9c5fd03c5cc35a28a9ce0849c4138259ae8e3d
Mani-official-10 <mani1993gusain@gmail.com>
2024-08-30 00:09:53 +0530

    New project and changes

---
+++ Projects/Color_changer/Color_changer/script.js
@@ -0,0 +1,46 @@
+const buttons = document.querySelectorAll('.boxes');
+// console.log(button);
+const body = document.querySelector('.main-container');
+// console.log(body);
+
+buttons.forEach(function (button) {
+    // console.log(button);
+    
+    // button.addEventListener('click',function (e) {
+    //     if (e.target.id === "one") {
+    //         body.style.backgroundColor = "#212131"            
+    //     }
+    //     if (e.target.id === "two") {
+    //         body.style.backgroundColor = "orangered"            
+    //     }
+    //     if (e.target.id === "three") {
+    //         body.style.backgroundColor = "yellowgreen"            
+    //     }
+    //     if (e.target.id === "four") {
+    //         body.style.backgroundColor = "pink"            
+    //     }
+    // })
+    button.addEventListener('click',function (e) {
+        
+        switch (e.target.id) {
+            case "one":
+                body.style.backgroundColor = "#212131";
+                break;
+            case "two":
+                body.style.backgroundColor = "orangered";
+                break;
+            case "three":
+                body.style.backgroundColor = "yellowgreen";
+                break;
+            case "four":
+                body.style.backgroundColor = "pink";
+                break;
+            default:
+                break;
+        }
+    })
+});
+
+
+
+
